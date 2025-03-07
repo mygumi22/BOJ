ff60cd68923fb55974dde9d1f59c8ac78e11ca3e
mygumi22 <mygumi22@gmail.com>
2025-03-07 17:35:17 +0900

    24060 풀이 추가

--- Silver/24060. 알고리즘 수업 - 병합 정렬 1/boj_24060.py
+++ Silver/24060. 알고리즘 수업 - 병합 정렬 1/boj_24060.py
@@ -1,2 +1,54 @@
 # 문제링크 https://www.acmicpc.net/problem/24060
 
+# A = [4, 5, 1, 3, 2]
+# K = 7
+# N = 5
+
+# A = [4, 5, 1, 3, 2]
+# K = 13
+# N = 5
+
+N, K = map(int, input().split())
+A = list(map(int, input().split()))
+T = []
+
+def merge_sort(A, low, high):
+  if high > low:
+    mid = (low + high) // 2
+    merge_sort(A, low, mid)
+    merge_sort(A, mid+1, high)
+    merge(A, low, mid, high)
+
+def merge(A, low, mid, high):
+  i = low
+  j = mid + 1
+  S = []
+
+  while (i <= mid and j <= high):
+    if A[i] <= A[j]:
+      S.append(A[i])
+      i += 1
+    else:
+      S.append(A[j])
+      j += 1
+  while (i <= mid):
+    S.append(A[i])
+    i += 1
+  while (j <= high):
+    S.append(A[j])
+    j += 1
+
+  i = low
+  t = 0
+  while (i <= high):
+    A[i] = S[t]
+    T.append(A[i])
+
+    i += 1
+    t += 1
+
+merge_sort(A, 0, len(A) - 1)
+if K > len(T):
+  print(-1)
+else:
+  print(T[K-1])
